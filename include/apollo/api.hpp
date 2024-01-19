89e611a7cb416e50d1758c0fb959d5f9153f63b6
Nikhil Gajjar <nikgajjar51@gmail.com>
2024-01-19 10:06:44 -0500

    Adding #includes for Units to api/hpp

--- include/apollo/api.hpp
+++ include/apollo/api.hpp
@@ -1,1 +1,25 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 #pragma once
+
+#include "apollo/units/QAcceleration.hpp "
+#include "apollo/units/QAngle.hpp "
+#include "apollo/units/QAngularAcceleration.hpp "
+#include "apollo/units/QAngularJerk.hpp "
+#include "apollo/units/QAngularSpeed.hpp "
+#include "apollo/units/QArea.hpp "
+#include "apollo/units/QForce.hpp "
+#include "apollo/units/QFrequency.hpp "
+#include "apollo/units/QJerk.hpp "
+#include "apollo/units/QLength.hpp "
+#include "apollo/units/QMass.hpp "
+#include "apollo/units/QPressure.hpp "
+#include "apollo/units/QSpeed.hpp "
+#include "apollo/units/QTime.hpp "
+#include "apollo/units/QTorque.hpp "
+#include "apollo/units/QVolume.hpp "
+#include "apollo/units/RQuantity.hpp "
+#include "apollo/units/RQuantityName.hpp
